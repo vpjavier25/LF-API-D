ec00d391983a0c4f96127cb1ca1001c99dd04ede
AlanSilcher <silcheralan@gmail.com>
2023-03-14 15:03:53 -0300

    Realize cambios para empezar a trabajar

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/linkingfuture`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -34,8 +34,8 @@ const { Recipe, Diet } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Recipe.belongsToMany(Diet, {through: 'recipe_diet'})
-Diet.belongsToMany(Recipe, {through: 'recipe_diet'})
+/* Recipe.belongsToMany(Diet, {through: 'recipe_diet'})
+Diet.belongsToMany(Recipe, {through: 'recipe_diet'}) */
 
 
 module.exports = {
