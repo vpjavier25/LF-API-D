dfb7fca0e6c4fc8b56f7f854c1dec29069385a17
Daniel Burgos <dburgos.developer@gmail.com>
2023-03-29 14:46:20 -0500

    google

--- api/src/controllers/GoogleCallBackController.js
+++ api/src/controllers/GoogleCallBackController.js
@@ -4,10 +4,10 @@ const { issueJWT } = require("../authWithJWT/utils")
 const GoogleCallBackController = (req, res) => {
     // Successful authentication, redirect home.
     const token = issueJWT(req.user);
-  
-    res.cookie("value", token.token, { httpOnly: false, maxAge: 1000 * 60 * 60 })
+    console.log("llegue aquí");
+    res.cookie("value", token.token, { domain: '.client-pf-seven.vercel.app', path: '/home', secure: true, httpOnly: false, maxAge: 1000 * 60 * 60 })
     res.cookie("success", "true", { httpOnly: false, maxAge: 500000000 }) 
-    console.log(res.cookie);
+    console.log(res.cookies.value);
     res.redirect('https://client-pf-seven.vercel.app/home');
 }
 
