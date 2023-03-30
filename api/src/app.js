ef3d2a348a8dd5831a0d6cc68e92402ed7729d7b
Daniel Burgos <dburgos.developer@gmail.com>
2023-03-30 10:36:10 -0500

    15.2

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ require('./authWithGoogle/passport')(passport);
 server.use(passport.initialize());
 
 server.use(cors({
-  origin: 'https://client-pf-seven.vercel.app/',
+  origin: 'https://client-pf-seven.vercel.app',
   credentials: true}))
 
 server.name = 'API';
